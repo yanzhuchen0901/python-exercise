948486ee61260916e52cae1eb5e7c597a5fc30f5
yanzhuchen0901 <yanzhuchen192@gmail.com>
2026-02-21 12:22:11 +0800

    全部完成，但是tinker还不是很懂

--- Exercise 05-05.py
+++ Exercise 05-05.py
@@ -7,3 +7,24 @@ sum = 0
 for i in price:
     sum += price[i] * quantity[i]
 print("Total cost:", sum)
+
+
+
+class fruit:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+    def sum(self):
+        return self.price * self.quantity
+    def print_answer(self):
+        print(self.name, self.price, self.quantity, self.sum())
+
+
+banana = fruit("Banana", 4, 1)
+apple = fruit("Apple", 2, 0)  
+orange = fruit("Orange", 1.5, 3)
+pear = fruit("Pear", 3, 2)
+
+sum = banana.sum() + apple.sum() + orange.sum() + pear.sum()
+print("Total cost:", sum)
